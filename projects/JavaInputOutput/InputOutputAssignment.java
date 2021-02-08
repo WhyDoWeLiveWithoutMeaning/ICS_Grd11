357403234dc0c467625edd7dd49571203366eb5d
Meaning <68759343+WhyDoWeLiveWithoutMeaning@users.noreply.github.com>
2021-02-07 22:38:58 -0500

    Update InputOutputAssignment.java

--- projects/JavaInputOutput/InputOutputAssignment.java
+++ projects/JavaInputOutput/InputOutputAssignment.java
@@ -19,13 +19,13 @@ public class InputOutputAssignment {
         System.out.print("What month was the Treaty of Versailles Signed?: ");
         String peaceTreaty = in.next();
 
-        System.out.print("What year did the French invasion of Russia begin?: ");
-        int invasionYear = in.nextInt();
+        System.out.print("What is the first letter of your great grandfathers last name.");
+        char invasionYear = in.next().charAt(0);
 
         System.out.println(String.format("You said there are %d bones in an Adult Human Body.", bones));
         System.out.println(String.format("You said it was %b that the war of 1812 ended in 1816.", war));
         System.out.println(String.format("You said the distance between the two points is %f.", distance));
         System.out.println(String.format("You said the month the Treaty of Versailles was signed was %s", peaceTreaty));
-        System.out.println(String.format("You said the French invasion of Russian began in %d", invasionYear));
+        System.out.println(String.format("You said the French invasion of Russian began in %s", invasionYear));
     }
 }
