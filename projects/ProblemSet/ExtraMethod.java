412aa325b1b634b30acace0821298d5c46bef4af
Meaning <68759343+WhyDoWeLiveWithoutMeaning@users.noreply.github.com>
2021-03-13 14:17:01 -0500

    Update ExtraMethod.java

--- projects/ProblemSet/ExtraMethod.java
+++ projects/ProblemSet/ExtraMethod.java
@@ -30,7 +30,7 @@ public class ExtraMethod {
     }
 
     public static int gcd(int n1, int n2){
-        for(int i = Math.min(n1, n2); i > 0; i++){
+        for(int i = Math.min(n1, n2); i > 0; i--){
             if(n1 % i == 0 && n2 % i == 0){
                 return i;
             }
